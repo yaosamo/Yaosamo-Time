35034c8c31765ffe17b8b4e8f974c8694cd994c6
Yaosamo user <yaosamo@gmail.com>
2026-02-24 10:44:21 -0800

    Initial Commit

---
+++ Yaosamo Time/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Yaosamo Time
+//
+//  Created by Personal on 2/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Yaosamo Time/Yaosamo_TimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yaosamo_TimeApp.swift
+//  Yaosamo Time
+//
+//  Created by Personal on 2/24/26.
+//
+
+import SwiftUI
+
+@main
+struct Yaosamo_TimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
